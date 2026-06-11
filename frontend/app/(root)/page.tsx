03d0b8e41a4c713899e442f558d1467170b6a5d5
HerbertNtim <herbertntim15@gmail.com>
2026-06-11 16:42:53 +0000

    starting the hero section again

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,34 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <main className="wrapper container">
-      <section className="hero-section">
-        <div className="hero-inner">
-          {/* LEFT */}
-          <div className="hero-text">
-            <h1 className="hero-title">
-              Meet Facelens <span>—</span> Your Gateway to Stunning Web
-              Experiences
-            </h1>
-            <Link href="/projects" className="hero-cta-primary">
-              Explore Projects
-            </Link>
-          </div>
-
-          {/* RIGHT */}
-          <div className="hero-image-frame">
-            <Image
-              src="/images/image_3-removebg-preview.png"
-              alt="FaceLens hero background"
-              fill
-              className="hero-image"
-              priority
-            />
-          </div>
-        </div>
-      </section>
-    </main>
-  );
+    <div>HI</div>
+  )
 }
