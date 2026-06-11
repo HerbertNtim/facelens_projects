223faf0212e620ffd33c9c66c6d9d22505eff075
HerbertNtim <herbertntim15@gmail.com>
2026-06-11 16:33:05 +0000

    positioning hero section well

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -4,27 +4,29 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="wrapper container">
-      <section className="wrapper flex gap-8 sm:gap-12 items-center">
-        {/* LEFT */}
-        <div className="hero-text">
-          <h1 className="hero-title">
-            Meet Facelens <span>—</span> Your Gateway to Stunning Web
-            Experiences
-          </h1>
-          <Link href="/projects" className="hero-cta-primary">
-            Explore Projects
-          </Link>
-        </div>
+      <section className="hero-section">
+        <div className="hero-inner">
+          {/* LEFT */}
+          <div className="hero-text">
+            <h1 className="hero-title">
+              Meet Facelens <span>—</span> Your Gateway to Stunning Web
+              Experiences
+            </h1>
+            <Link href="/projects" className="hero-cta-primary">
+              Explore Projects
+            </Link>
+          </div>
 
-        {/* RIGHT */}
-        <div className="hero-image-frame">
-          <Image
-            src="/images/image_1.jpg"
-            alt="FaceLens hero background"
-            fill
-            className="hero-image"
-            priority
-          />
+          {/* RIGHT */}
+          <div className="hero-image-frame">
+            <Image
+              src="/images/image_3-removebg-preview.png"
+              alt="FaceLens hero background"
+              fill
+              className="hero-image"
+              priority
+            />
+          </div>
         </div>
       </section>
     </main>
