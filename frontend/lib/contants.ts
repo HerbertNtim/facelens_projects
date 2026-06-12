75afaae4594ccdb7405ad4683eb82bae0f1078ee
HerbertNtim <herbertntim15@gmail.com>
2026-06-12 17:27:54 +0000

    set up constants for projects

---
+++ frontend/lib/contants.ts
@@ -0,0 +1,9 @@
+import { projectIcons } from "./icons";
+
+export const projects = [
+  {
+    title: 'Gender Prediction',
+    description: 'A project that predicts the gender of a person based on their facial features using machine learning algorithms.',
+    icon: projectIcons.icon_1
+  }
+]
