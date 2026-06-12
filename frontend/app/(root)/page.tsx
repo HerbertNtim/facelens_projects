a440624d1d1b485cd0413222f7202a9278fe162d
HerbertNtim <herbertntim15@gmail.com>
2026-06-12 16:39:26 +0000

    separating css from ui code

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <main className="flex flex-col bg-(--bg-light) dark:bg-(--bg-dark)">
-      <section className="relative h-screen z-10 overflow-hidden pb-16 pt-30 md:pb-30 md:pt-37.5 xl:pb-40 xl:pt-45 2xl:pb-50 2xl:pt-52.5">
+    <main className="page">
+      <section className="page-section">
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
-              <div className="mx-auto max-w-200 text-center py-8 sm:py-12 lg:py-16">
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+              <div className="mx-auto max-w-200 text-center px-4 py-8 sm:py-12 lg:py-16">
+                <h1 className="page-title">
                   Explore the World Through Faces{" "}
                 </h1>
-                <p className="mb-12 text-base leading-relaxed! text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+                <p className="page-description">
                   Facelens is an open portfolio of computer vision projects — built by developers, researchers, and creators pushing the boundaries of facial recognition, detection, and analysis.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
@@ -35,6 +35,8 @@ const Home = () => {
         </div>
         <HeroBackground />
       </section>
+
+
     </main>
   );
 };
