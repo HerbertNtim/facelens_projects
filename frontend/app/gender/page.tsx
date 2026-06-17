27d4fc925a0a5ce4142301a72f81d4ff84a3c1fb
HerbertNtim <herbertntim15@gmail.com>
2026-06-17 11:23:28 +0000

    form title

--- frontend/app/gender/page.tsx
+++ frontend/app/gender/page.tsx
@@ -1,9 +1,14 @@
 const GenderPage = () => {
   return (
-    <main className='page'>
-      <section className='page-section'>
-        Gender Prediction Page
-      </section>
+    <main className='wrapper container'>
+      <div className='mx-auto max-w-180 space-y-10 py-20'>
+        <section className='flex flex-col gap-5 text-center'>
+          <h1 className='text-3xl font-bold'>Gender Prediction</h1>
+          <p className='text-lg text-muted-foreground'>
+            Upload an image of a face to predict the gender.
+          </p>
+        </section>
+      </div>
     </main>
   )
 }
