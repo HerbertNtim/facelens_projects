67346c450e77cd65ee26f36e2ed9ac1f3765bd31
HerbertNtim <herbertntim15@gmail.com>
2026-06-17 15:00:07 +0000

    loading ui

--- frontend/components/LoadingOverlay.tsx
+++ frontend/components/LoadingOverlay.tsx
@@ -6,13 +6,12 @@ import { Loader2 } from 'lucide-react';
 const LoadingOverlay = () => {
   return (
     <div className="loading-wrapper">
-      <div className="loading-shadow-wrapper bg-white shadow-soft-lg">
+      <div className="loading-shadow-wrapper bg-white dark:bg-(--bg-dark) shadow-soft-lg">
         <div className="loading-shadow">
-          <Loader2 className="loading-animation w-12 h-12 text-[#663820]" />
-          <h2 className="loading-title">Synthesizing Your Book</h2>
+          <Loader2 className="loading-animation w-12 h-12 text-primary" />
+          <h2 className="loading-title">Synthesizing Your Image</h2>
           <p className="text-[#777] dark:text-(--text-secondary) text-center max-w-xs">
-            Please wait while we process your PDF and prepare your interactive
-            literary experience.
+            Please wait while we process your Image and make prediction.
           </p>
         </div>
       </div>
